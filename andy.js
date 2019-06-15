217383c9bb8fc0633f3366552284d28a0b1c02e5
Edmond Natweta <araali256@gmail.com>
2019-06-14 20:12:01 -0700

    addind andy.js to the repo

--- andy.js
+++ andy.js
@@ -1,3 +1,13 @@
 var name = "Andrew"
+var msg = "Go to school young man"
+var msg2= "I think andy shulld get a wife"
 var age= 56
-console.log(name +"   Thats my name and am    "+ age+ " years old" )
+var year = 2019
+console.log(name +"   Thats my name and am    "+ age+ " years old \n" )
+console.log("you  were born in "+ year-age)
+
+if (age>78){
+    console.log(msg)
+}
+else
+console.log(msg2)
